7fb1aa9b5fcfb686a349e814cc8f4315527da5f4
avielas@gmail.com <avielas@gmail.com>
2018-06-23 23:34:18 +0300

    add Ch08Ex01 - CriminalIntent

--- Ch08Ex01 - CriminalIntent/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ Ch08Ex01 - CriminalIntent/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,13 +1,14 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
---
+++ Ch08Ex01 - CriminalIntent/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,47 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public static CrimeLab get(Context context){
+        if(null == sCrimeLab)
+        {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public void setCrimes(List<Crime> crimes) {
+        mCrimes = crimes;
+    }
+
+    public Crime getCrime(UUID uuid){
+        for(Crime crime : mCrimes){
+            if(crime.getId().equals(uuid)){
+                return crime;
+            }
+        }
+        return null;
+    }
+}
