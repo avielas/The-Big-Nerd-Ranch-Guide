f0d544af9c3cf587bdea7ea4fcff36c37bfbdc64
avielas@gmail.com <avielas@gmail.com>
2018-07-06 16:02:51 +0300

    Ch10Ex01 - CriminalIntent

---
+++ Ch10Ex01 - CriminalIntent/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.UUID;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID =
+            "com.bignerdranch.android.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
